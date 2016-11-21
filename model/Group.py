658e15ed5e2c6f3099dae2d53f861a6d317f8909
talareq <galuszka.grzegorz@yahoo.com>
2016-11-21 15:07:47 +0000

    exctacted new class SessionHelper

--- model/Group.py
+++ model/Group.py
@@ -1,4 +1,6 @@
 class Group:
-    def __init__(self, firstname, lastname):
-        self.firstname=firstname
-        self.lastname=lastname
+
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,21 +7,20 @@ from fixture.application import Application
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinilizer(fixture.destroy)
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_add_group(app):
 
-    app.login(wd, username="admin", password="secret")
-    app.open_groups_page(wd)
-    app.group_creation(wd, Group(name="wada", header="\\65", footer="afsdcas"))
-    app.logout(wd)
+    app.session.login(username="admin", password="secret")
+    app.group_creation(Group(name="wada", header="\\65", footer="afsdcas"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
 
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.group_creation( Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
