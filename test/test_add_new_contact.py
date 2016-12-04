a3e134e1b29aa57f9fc9d95312dd9440cde9921e
talareq <galuszka.grzegorz@yahoo.com>
2016-12-04 13:45:24 +0000

    Revert "Revert "working at homework 11""
    This reverts commit d60f767d79c62c5bdf2bdff12f12311db183bc8e.

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -2,6 +2,11 @@ from model.formfiller import Contact
 
 def test_add_new_contact(app):
 
-    app.contact.add_new_contact(Contact(firstname="g",lastname="wadwa"))
-
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="g",lastname="wadwa")
+    app.contact.add_new_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -5,11 +5,21 @@ from model.formfiller import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.modify_first_group(Group(name="New Group"))
+    group = Group(name="New Group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_group_header(app):
-   app.group.modify_first_group(Group(header="New Header"))
+#def test_modify_group_header(app):
+#    old_groups = app.group.get_group_list()
+#    app.group.modify_first_group(Group(header="New Header"))
+ #   new_groups = app.group.get_group_list()
+ #   assert len(old_groups) == len(new_groups)
 
 
