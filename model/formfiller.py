f98a5bb3b8d1c236d89ccc18deeb7d92de5f2bfb
talareq <galuszka.grzegorz@yahoo.com>
2016-12-11 23:55:08 +0000

    Homework 14
    Checking contact details at edit and home page
    assertion error

--- model/formfiller.py
+++ model/formfiller.py
@@ -26,11 +26,23 @@ class Group:
 
 class Contact:
 
-    def __init__(self, firstname=None, middlename=None, lastname=None, id=None):
+    def __init__(self, firstname=None, lastname=None, homephone=None,
+                 mobilephone=None, workphone=None, secondaryphone=None,
+                 id=None, all_phones_from_home_page=None, all_emails_from_home_page=None,
+                 email=None, email2=None, email3=None, adress=None):
         self.firstname=firstname
-        self.middlename=middlename
         self.lastname=lastname
+        self.homephone=homephone
+        self.mobilephone=mobilephone
+        self.workphone=workphone
+        self.secondaryphone=secondaryphone
         self.id = id
+        self.all_phones_from_home_page=all_phones_from_home_page
+        self.all_emails_from_home_page=all_emails_from_home_page
+        self.email=email
+        self.email2=email2
+        self.email3=email3
+        self.adress=adress
 
     def __repr__(self):
         return "%s:%s" % (self.id, self.firstname)
@@ -38,7 +50,7 @@ class Contact:
 
     def __eq__(self, other):
         #return self.id == other.id and self.name == other.name
-        return (self.id is None or other.id is None or self.id== other.id) and self.firstname == other.firstname
+        return (self.id is None or other.id is None or self.id== other.id) and self.firstname == other.firstname and self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:
