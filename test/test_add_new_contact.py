784d931d4324d2837df1b466dcb2bf8d876e5984
talareq <galuszka.grzegorz@yahoo.com>
2016-12-12 12:25:28 +0000

    Homework 14 continued

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,9 +1,26 @@
 from model.formfiller import Contact
+import pytest
+import random
+import string
 
-def test_add_new_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+
+testdata = [Contact(firstname="", lastname="", adress="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 20), adress=random_string("address", 20))
+    for i in range(5)
+]
+
+
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_contact(app, contact):
 
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="g",lastname="wadwa")
     app.contact.add_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
