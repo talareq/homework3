961d4736e676ade8136c1311f9154cf10b239017
talareq <galuszka.grzegorz@yahoo.com>
2017-01-22 17:52:27 +0000

    cd2

--- rf/AddressBook.py
+++ rf/AddressBook.py
@@ -17,15 +17,25 @@ class AddressBook:
 
     def init_fixtures(self):
         web_config = self.target["web"]
-        self.fixtures = Application(browser=self.browser, base_url=web_config["baseUrl"])
+        self.fixture = Application(browser=self.browser, base_url=web_config["baseUrl"])
+        self.fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
         db_config = self.target["db"]
         self.dbfixture = Dbfixture(host=db_config["host"], name=db_config["name"], user=db_config["user"],
                               password=db_config["password"])
 
     def destroy_fixtures(self):
         self.dbfixture.destroy()
-        self.fixtures.destroy()
+        self.fixture.destroy()
+
+    def get_group_list(self):
+        return self.dbfixture.get_group_list()
+
+    def new_group(self, name, header, footer):
+        return (Group(name=name, header=header, footer= footer))
 
 
-    def create_group(self, name, header, footer):
-        self.fixtures.group.create(Group(name=name, header=header, footer= footer))
+    def group_list_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
+
+    def create_group(self, group):
+        self.fixture.group.create(group)
--- rf/group_scenarios.robot
+++ rf/group_scenarios.robot
@@ -1,10 +1,16 @@
 *** Settings ***
-Library rf.AddressBook
-Suite Setup Init Fixtures
-Suite Teardown Destroy Fixtures
+Library  rf.AddressBook
+Library  Collections
+Suite Setup  Init Fixtures
+Suite Teardown  Destroy Fixtures
 
 
 
 *** Test Cases ***
 Add New group
-    Create Group  name1  header1  footer1
+    $(old_list)=   Get Group List
+    $(group)=  New Group  name1  header1  footer1
+    Create Group  $(group)
+    $(new_list)=  Get Group List
+    Append To List   $(old_list)  $(group)
+    Group List Should Be Equal  $(new_list)  $(old_list)
